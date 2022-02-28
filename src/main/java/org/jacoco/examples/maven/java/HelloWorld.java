a35a6de4786e9b13675bb0f4bdf162adfb81d698
ba1997 <100507173+ba1997@users.noreply.github.com>
2022-02-28 06:21:07 +0530

    Update HelloWorld.java

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -1,6 +1,6 @@
 package org.jacoco.examples.maven.java;//variable test webhooks
 
-public class HelloWorld {
+public class HelloWorld { //variable
 
 	public String getMessage(boolean bigger) {
 		if (bigger) {
